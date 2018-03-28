d21afc45e213b03651848b5a348b6f3fb2e79926
Chris Holder <chris@techlinksolutions.com>
2018-03-28 13:36:51 -0400

    Clean up code, add comments

--- js/main.js
+++ js/main.js
@@ -4,29 +4,37 @@ const students = page.querySelectorAll('.student-item');
 const pageHeader = page.querySelector('.page-header');
 const pageSize = 10;
 
+// Takes an array and returns an object with a number of pages containing no more than "pageSize" items
+// Returned obj also contains total number of pages
 function populatePages(list) {
   const pages = {};
   pages.numOfPages = Math.ceil(list.length / pageSize);
-  let pageIndex = 0;
+  let itemIndex = 0;
 
+  // Create a page property containing "pageSize" number of items
+  // Slice the list down by incrementing the "itemIndex" by the "pageSize"
+  // Grab the remainder of items when the loop is on its last pass
   for (let i = 1; i <= pages.numOfPages; i += 1) {
-    pages[`page${i}`] = list.slice(pageIndex, (i < pages.numOfPages) ? pageIndex += pageSize : list.length);
+    pages[`page${i}`] = list.slice(itemIndex, (i < pages.numOfPages) ? itemIndex += pageSize : list.length);
   }
+
   return pages;
 }
 
-function addPaginationElem(parentNode) {
+// Adds the pagination div and ul to the DOM
+function addPaginationBlock(parentNode) {
   const paginationDiv = document.createElement('div');
   paginationDiv.className = 'pagination';
-  const paginationList = document.createElement('ul');
-  paginationDiv.appendChild(paginationList);
-  parentNode.appendChild(paginationDiv);
+  paginationDiv.appendChild(document.createElement('ul'));
+  return parentNode.appendChild(paginationDiv);
 }
 
+// Adds the pagination links to the DOM
 function addPageLinks(pagesObj, parentNode) {
-  for (let i = 1; i <= pages.numOfPages; i += 1) {
+  for (let i = 1; i <= pagesObj.numOfPages; i += 1) {
     const paginationItem = document.createElement('li');
     const paginationLink = document.createElement('a');
+    // Always load the first page
     if (i === 1) {
       paginationLink.classList.add('active');
     }
@@ -37,11 +45,16 @@ function addPageLinks(pagesObj, parentNode) {
   }
 }
 
-function loadPage(pageNum, list) {
-  const studentNodes = list[`page${pageNum}`];
+// Adds the list items for a given page to the DOM
+function loadPage(pageNum, pagesObj) {
+  const studentNodes = pagesObj[`page${pageNum}`];
+  // Clear out the list
   studentList.innerHTML = '';
+
+  // Set up a cascading fade-in effect, adding 20ms delay for each list item
   let delay = 20;
   studentNodes.forEach((studentNode) => {
+    // Remove the show class so items will always fade in
     studentNode.classList.remove('show');
     studentList.appendChild(studentNode);
     setTimeout(() => {
@@ -51,17 +64,19 @@ function loadPage(pageNum, list) {
   });
 }
 
-const pages = populatePages([...students]);
-addPaginationElem(page);
-addPageLinks(pages, page.querySelector('.pagination ul'));
 
-const paginationDiv = page.querySelector('.pagination');
-paginationDiv.addEventListener('click', (e) => {
+// Initial page load
+// Initialize the pages obj with array of students pulled from the html
+const pagesObj = populatePages([...students]);
+addPageLinks(pagesObj, addPaginationBlock(page));
+loadPage(1, pagesObj);
+
+// Event listeners
+// Pagination page selection
+page.querySelector('.pagination').addEventListener('click', (e) => {
   if (e.target.nodeName === 'A') {
     page.querySelector('.active').classList.remove('active');
     e.target.classList.add('active');
-    loadPage(e.target.innerText, pages);
+    loadPage(e.target.innerText, pagesObj);
   }
 });
-
-loadPage(1, pages);
